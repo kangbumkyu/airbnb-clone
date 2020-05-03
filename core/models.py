7f4b7bc51e359e45722e90963e3240e8c2a98a1b
Kangbumkyu <g.kangbumkyu@gmail.com>
2020-05-03 21:44:07 +0700

    Create Room App Model

--- core/models.py
+++ core/models.py
@@ -7,8 +7,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     # Meta 클래스는 TimeStampedModel이 실제 DB에 생성되는게 아니라 다른 클래스에서 상속받기 위해서 사용함.
     class Meta:
--- rooms/admin.py
+++ rooms/admin.py
@@ -4,7 +4,22 @@ from . import models
 # Register your models here.
 
 
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Description """
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """ Room Admin Description """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo Admin Description """
 
     pass
