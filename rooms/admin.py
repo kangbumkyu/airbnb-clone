6c415c3ebffcc1ca9c04f17ffc022b70d332a7ee
Kangbumkyu <g.kangbumkyu@gmail.com>
2020-05-04 20:31:55 +0700

    Add column for romm admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -8,7 +8,13 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """ Item Admin Description """
 
-    pass
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -44,6 +50,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -67,6 +74,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
