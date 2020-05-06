9a3caebbfc071a475caa2c8ed062884f904566c1
Kangbumkyu <g.kangbumkyu@gmail.com>
2020-05-06 23:05:35 +0700

    Modify Save() method

--- rooms/admin.py
+++ rooms/admin.py
@@ -18,14 +18,21 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     """ Room Admin Description """
 
+    inlines = (PhotoInline,)
+
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")},
+            {"fields": ("name", "description", "country", "city", "address", "price")},
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
@@ -66,7 +73,9 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
     )
 
-    search_fields = ("^city", "^host__username")
+    raw_id_fields = ("host",)
+
+    search_fields = ("=city", "^host__username")
 
     filter_horizontal = (
         "amenities",
